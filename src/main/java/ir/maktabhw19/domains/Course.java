d98c6eda90668b7a4a2b4355364d508a177f546d
Maryam <maryamhabibi868@gmail.com>
2025-05-05 20:43:44 +0330

    WIP: add entities class

---
+++ src/main/java/ir/maktabhw19/domains/Course.java
@@ -0,0 +1,17 @@
+package ir.maktabhw19.domains;
+
+import ir.maktabhw19.domains.base.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class Course extends BaseEntity<Long> {
+}
