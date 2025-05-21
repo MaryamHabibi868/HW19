dddbb977f19529105511cacbe8777a91862230e9
Maryam <maryamhabibi868@gmail.com>
2025-05-21 20:11:09 +0330

    feat: add calculate score to exam in entity

--- src/main/java/ir/maktabhw19/domains/Exam.java
+++ src/main/java/ir/maktabhw19/domains/Exam.java
@@ -1,6 +1,7 @@
 package ir.maktabhw19.domains;
 
 import ir.maktabhw19.domains.base.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
@@ -8,6 +9,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -19,7 +21,7 @@ import java.util.Set;
 @ToString(callSuper = true)
 public class Exam extends BaseEntity<Long> {
 
-    private Double score;
+    private Double givenScore;
 
     @NotBlank (message = "Start Date of Exam should be entered")
     private LocalDateTime startDate;
@@ -30,8 +32,36 @@ public class Exam extends BaseEntity<Long> {
     @ManyToOne
     private Course course;
 
-    @OneToMany (mappedBy = "exam")
-    private Set<Question> questions;
+    @OneToMany (mappedBy = "exam" , cascade = CascadeType.ALL)
+    private Set<Question> questions = new HashSet<>();
 
+  public Double calculateDefaultExamScore() {
+      if (questions == null || questions.isEmpty()) {
+          return 0.0;
+      }
+      return questions.stream()
+              .filter(q -> q.getDefaultScore() != null)
+              .mapToDouble(Question :: getDefaultScore)
+              .sum();
+  }
 
+    public Double calculateGivenScore() {
+        if (questions == null || questions.isEmpty()) {
+            return 0.0;
+        }
+        return questions.stream()
+                .filter(q -> q.getGivenScore() != null)
+                .mapToDouble(Question :: getGivenScore)
+                .sum();
+    }
+
+    public void addQuestion(Question question) {
+        questions.add(question);
+        question.setExam(this);
+    }
+
+    public void removeQuestion(Question question) {
+        questions.remove(question);
+        question.setExam(null);
+    }
 }
