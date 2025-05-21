4c5239020d804c4ac2c1dcd0ab4d38ccd5b34bee
Maryam <maryamhabibi868@gmail.com>
2025-05-21 20:10:43 +0330

    feat: add validate anotaion to question classes

--- src/main/java/ir/maktabhw19/domains/MultipleChoiceQuestion.java
+++ src/main/java/ir/maktabhw19/domains/MultipleChoiceQuestion.java
@@ -3,6 +3,7 @@ package ir.maktabhw19.domains;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 import java.util.ArrayList;
@@ -21,6 +22,7 @@ public class MultipleChoiceQuestion extends Question {
     @ElementCollection
     private List<String> options = new ArrayList<>();
 
+    @NotBlank(message = "Index of correct option should be entered")
     private Integer correctOptionIndex;
 
     public String getCorrectAnswer() {
--- src/main/java/ir/maktabhw19/domains/Question.java
+++ src/main/java/ir/maktabhw19/domains/Question.java
@@ -2,6 +2,7 @@ package ir.maktabhw19.domains;
 
 import ir.maktabhw19.domains.base.BaseEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -12,15 +13,20 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString(callSuper = true)
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "question_type")
 public class Question extends BaseEntity <Long> {
 
+    @NotBlank (message = "Title of question should be entered")
     private String questionTitle;
 
+    @NotBlank (message = "Statement of question should be entered")
     private String questionStatement;
 
+    @NotBlank (message = "Default score of question should be entered")
     private Double defaultScore;
+
+    private Double givenScore;
 
     @ManyToOne
     private Exam exam;
