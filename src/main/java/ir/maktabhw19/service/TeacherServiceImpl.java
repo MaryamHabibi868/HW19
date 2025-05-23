fe361196660d178922b75d71017fa9d94d1a4732
Maryam <maryamhabibi868@gmail.com>
2025-05-23 19:42:06 +0330

    feat: add addQuestion for TeacherServiceImpl

--- src/main/java/ir/maktabhw19/service/TeacherServiceImpl.java
+++ src/main/java/ir/maktabhw19/service/TeacherServiceImpl.java
@@ -1,33 +1,32 @@
 package ir.maktabhw19.service;
 
-import ir.maktabhw19.domains.Exam;
-import ir.maktabhw19.domains.RegistrationConfirmation;
-import ir.maktabhw19.domains.Student;
-import ir.maktabhw19.domains.Teacher;
+import ir.maktabhw19.domains.*;
 import ir.maktabhw19.repository.TeacherRepository;
 import ir.maktabhw19.service.base.BaseServiceImpl;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class TeacherServiceImpl
         extends BaseServiceImpl<Teacher, Long, TeacherRepository>
-        implements TeacherService{
+        implements TeacherService {
 
     public TeacherServiceImpl(TeacherRepository repository,
                               ExamService examService,
-                              DescriptiveQuestionsService descriptiveQuestionsService,
+                              DescriptiveQuestionService descriptiveQuestionService,
                               MultipleChoiceQuestionService multipleChoiceQuestionService,
                               QuestionService questionService) {
         super(repository);
         this.examService = examService;
-        this.descriptiveQuestionsService = descriptiveQuestionsService;
+        this.descriptiveQuestionService = descriptiveQuestionService;
         this.multipleChoiceQuestionService = multipleChoiceQuestionService;
         this.questionService = questionService;
     }
 
     private final ExamService examService;
-    private final DescriptiveQuestionsService descriptiveQuestionsService;
+    private final DescriptiveQuestionService descriptiveQuestionService;
     private final MultipleChoiceQuestionService multipleChoiceQuestionService;
     private final QuestionService questionService;
 
@@ -65,8 +64,7 @@ public class TeacherServiceImpl
             } else {
                 System.out.println("Wrong password");
             }
-        }
-        else {
+        } else {
             System.out.println("userName not found");
         }
     }
@@ -74,13 +72,53 @@ public class TeacherServiceImpl
     public void addExamByTeacher(Long teacherId, LocalDateTime startDate,
                                  LocalDateTime endDate) {
         repository.beginTransaction();
-       if(repository.findById(teacherId).isEmpty()){
-           throw new RuntimeException("Teacher not found");
-       }
-       Exam exam = new Exam();
-       exam.setStartDate(startDate);
-       exam.setEndDate(endDate);
-       examService.save(exam);
-       repository.commitTransaction();
+        if (repository.findById(teacherId).isEmpty()) {
+            throw new RuntimeException("Teacher not found");
+        }
+        Exam exam = new Exam();
+        exam.setStartDate(startDate);
+        exam.setEndDate(endDate);
+        examService.save(exam);
+        repository.commitTransaction();
+    }
+
+    public void addDescriptiveQuestionsByTeacher(
+            Long teacherID, String questionTitle,
+            String questionStatement,
+            Double defaultScore) {
+        repository.beginTransaction();
+        if (repository.findById(teacherID).isEmpty()) {
+            throw new RuntimeException("Teacher not found");
+        }
+        DescriptiveQuestion question = new DescriptiveQuestion();
+        question.setQuestionTitle(questionTitle);
+        question.setQuestionStatement(questionStatement);
+        question.setDefaultScore(defaultScore);
+        descriptiveQuestionService.save(question);
+        repository.commitTransaction();
+        System.out.println("Descriptive Question added successfully");
+    }
+
+    public void addMultipleChoiceQuestionsByTeacher(
+            Long teacherID, String questionTitle,
+            String questionStatement,
+            Double defaultScore,
+            List<String> options,
+            Integer correctOptionIndex){
+        repository.beginTransaction();
+        if (repository.findById(teacherID).isEmpty()) {
+            throw new RuntimeException("Teacher not found");
+        }
+        MultipleChoiceQuestion question = new MultipleChoiceQuestion();
+        question.setQuestionTitle(questionTitle);
+        question.setQuestionStatement(questionStatement);
+        question.setDefaultScore(defaultScore);
+        List<String> optionsList = new ArrayList<>();
+        optionsList.addAll(options);
+        question.setOptions(optionsList);
+        question.setCorrectOptionIndex(correctOptionIndex);
+        multipleChoiceQuestionService.save(question);
+        repository.commitTransaction();
+        System.out.println("Multiple Choice Question added successfully");
     }
 }
