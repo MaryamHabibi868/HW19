322a61bb337df7a483c596726c1a50cd8c830bc3
Maryam <maryamhabibi868@gmail.com>
2025-05-27 22:15:37 +0330

    feat: add answerToDQ methods in AnswerToDQServiceImpl class

--- src/main/java/ir/maktabhw19/service/AnswerToDQServiceImpl.java
+++ src/main/java/ir/maktabhw19/service/AnswerToDQServiceImpl.java
@@ -7,7 +7,27 @@ import ir.maktabhw19.service.base.BaseServiceImpl;
 public class AnswerToDQServiceImpl
         extends BaseServiceImpl<AnswerToDQ, Long, AnswerToDQRepository>
         implements AnswerToDQService{
-    public AnswerToDQServiceImpl(AnswerToDQRepository repository) {
+    public AnswerToDQServiceImpl(AnswerToDQRepository repository,
+                                 StudentService studentService, DescriptiveQuestionService descriptiveQuestionService) {
         super(repository);
+        this.studentService = studentService;
+        this.descriptiveQuestionService = descriptiveQuestionService;
+    }
+
+    private final StudentService studentService;
+    private final DescriptiveQuestionService descriptiveQuestionService;
+
+    public void answerToDQ(Long studentId, Long questionId, String answer) {
+        repository.beginTransaction();
+        if (studentService.findById(studentId).isEmpty()){
+            throw new RuntimeException("Student not found");
+        }
+        if (descriptiveQuestionService.findById(questionId).isEmpty()){
+            throw new RuntimeException("Question not found");
+        }
+        AnswerToDQ answerToDQ = new AnswerToDQ();
+        answerToDQ.setAnswer(answer);
+        repository.save(answerToDQ);
+        repository.commitTransaction();
     }
 }
