7c3ed3a41ad18bee20597ea866a93a91117ae677
Maryam <maryamhabibi868@gmail.com>
2025-05-27 22:31:58 +0330

    feat: add answerToMCQ methods in AnswerToMCQServiceImpl class

--- src/main/java/ir/maktabhw19/service/AnswerToMCQServiceImpl.java
+++ src/main/java/ir/maktabhw19/service/AnswerToMCQServiceImpl.java
@@ -7,7 +7,30 @@ import ir.maktabhw19.service.base.BaseServiceImpl;
 public class AnswerToMCQServiceImpl
         extends BaseServiceImpl<AnswerToMCQ, Long, AnswerToMCQRepository>
         implements AnswerToMCQService {
-    public AnswerToMCQServiceImpl(AnswerToMCQRepository repository) {
+    public AnswerToMCQServiceImpl(AnswerToMCQRepository repository,
+                                  StudentService studentService,
+                                  MultipleChoiceQuestionService multipleChoiceQuestionService) {
         super(repository);
+        this.studentService = studentService;
+        this.multipleChoiceQuestionService = multipleChoiceQuestionService;
+    }
+
+    private final StudentService studentService;
+    private final MultipleChoiceQuestionService multipleChoiceQuestionService;
+
+    public void answerToMCQ(Long studentId, Long questionId,
+                            Integer selectedOption) {
+        repository.beginTransaction();
+        if (studentService.findById(studentId).isEmpty()){
+            throw new RuntimeException("Student not found");
+        }
+        if (multipleChoiceQuestionService.findById(questionId).isEmpty()){
+            throw new RuntimeException("Question not found");
+        }
+        AnswerToMCQ answerToMCQ = new AnswerToMCQ();
+        answerToMCQ.setSelectedOption(selectedOption);
+        answerToMCQ.calculateGivenScore();
+        repository.save(answerToMCQ);
+        repository.commitTransaction();
     }
 }
